fa5f84d5a86727ae4d9cad71095748cc1f3eed3a
kobayashi-m42 <m42kobayashi@gmail.com>
2018-08-31 01:22:34 +0900

    画面に必要なコンポーネントを作成

---
+++ src/components/modules/StringComp.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>{{ string }}</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class stringComp extends Vue {
+  string: string = "入力された感想をここに出す";
+}
+</script>
