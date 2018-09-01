05f200e69290bf147f2e96484cf6fec6c3a17c0b
kobayashi-m42 <m42kobayashi@gmail.com>
2018-09-02 01:05:04 +0900

    Todoアプリに必要なコンポーネントを作成

---
+++ src/components/modules/MainSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="main">
+  <input
+    class="toggle-all"
+    id="toggle-all"
+    type="checkbox"
+  >
+  <label for="toggle-all"></label>
+  <ul class="todo-list">
+    <TodoItem></TodoItem>
+    <TodoItem></TodoItem>
+  </ul>
+</section>
+  
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TodoItem from "./TodoItem.vue";
+
+@Component({
+  components: {
+    TodoItem
+  }
+})
+export default class MainSection extends Vue {}
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,26 @@
+<style src="todomvc-app-css/index.css">
+</style>
+
+<template>
+  <section class="todoapp">
+    <AppHeader></AppHeader>
+    <MainSection></MainSection>
+    <AppFooter></AppFooter>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import AppHeader from "@/components/modules/AppHeader.vue";
+import MainSection from "@/components/modules/MainSection.vue";
+import AppFooter from "@/components/modules/AppFooter.vue";
+
+@Component({
+  components: {
+    AppHeader,
+    MainSection,
+    AppFooter
+  }
+})
+export default class Todo extends Vue {}
+</script>
