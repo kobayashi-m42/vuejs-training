d6384f61228c8cfc93ad6e42a1fe82636a45b687
kobayashi-m42 <m42kobayashi@gmail.com>
2018-09-02 19:38:23 +0900

    Totoの編集機能を追加

--- src/components/modules/TodoItem.vue
+++ src/components/modules/TodoItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="todo" :class="{ completed: todo.done }">
+  <li class="todo" :class="{ completed: todo.done , editing: editing}">
     <div class="view">
       <input
         class="toggle"
@@ -7,9 +7,18 @@
         :checked="todo.done"
         @change="toggleTodo(todo)"
       >
-      <label v-text="todo.title"></label>
+      <label v-text="todo.title" @dblclick="editing = true"></label>
       <button class="destroy" @click="removeTodo(todo)"></button>
     </div>
+    <input
+      class="edit"
+      v-show="editing"
+      v-focus="editing"
+      :value="todo.title"
+      @keyup.enter="doneEdit"
+      @keyup.esc="cancelEdit"
+      @blur="doneEdit"
+    >
   </li>
 </template>
 
@@ -17,10 +26,20 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { ITodo } from "../../types/todo";
 
-@Component
+@Component({
+  directives: {
+    focus: function(el, binding) {
+      if (binding.value) {
+        el.focus();
+      }
+    }
+  }
+})
 export default class TodoItem extends Vue {
   @Prop()
   private todo!: ITodo;
+
+  editing: boolean = false;
 
   toggleTodo(todo: ITodo): void {
     this.$store.dispatch("toggleTodo", todo);
@@ -28,6 +47,25 @@ export default class TodoItem extends Vue {
 
   removeTodo(todo: ITodo): void {
     this.$store.dispatch("removeTodo", todo);
+  }
+
+  doneEdit(e: any): void {
+    const changedTitle: string = e.target.value.trim();
+    const todo: ITodo = this.todo;
+    if (!changedTitle) {
+      this.$store.dispatch("removeTodo", todo);
+    } else if (this.editing) {
+      this.$store.dispatch("editTodo", {
+        todo,
+        changedTitle
+      });
+      this.editing = false;
+    }
+  }
+
+  cancelEdit(e: any): void {
+    e.target.value = this.todo.title;
+    this.editing = false;
   }
 }
 </script>
