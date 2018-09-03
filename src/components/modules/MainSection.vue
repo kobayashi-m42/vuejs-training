bdb45e50b044bf8c84f6e5a1824d6ace51908851
kobayashi-m42 <m42kobayashi@gmail.com>
2018-09-03 23:22:11 +0900

    全てのTodoの完了状態を一括で更新する機能を追加

--- src/components/modules/MainSection.vue
+++ src/components/modules/MainSection.vue
@@ -1,14 +1,16 @@
 <template>
-  <section class="main">
+  <section class="main" v-show="allTodos.length" >
     <input
       class="toggle-all"
       id="toggle-all"
       type="checkbox"
+      :checked="allChecked"
+      @change="toggleAll(!allChecked)"
     >
     <label for="toggle-all"></label>
     <ul class="todo-list">
       <TodoItem
-        v-for="(todo, index) in todos"
+        v-for="(todo, index) in filteredTodos"
         :key="index"
         :todo="todo"
       ></TodoItem>
@@ -27,8 +29,20 @@ import { ITodo } from "../../types/todo";
   }
 })
 export default class MainSection extends Vue {
-  get todos(): ITodo[] {
+  get filteredTodos(): ITodo[] {
     return this.$store.getters.filteredTodos;
+  }
+
+  get allTodos(): ITodo[] {
+    return this.$store.getters.all;
+  }
+
+  get allChecked() {
+    return this.allTodos.every(todo => todo.done);
+  }
+
+  toggleAll(allChecked: boolean): void {
+    this.$store.dispatch("toggleAll", allChecked);
   }
 }
 </script>
